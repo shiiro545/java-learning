9ed2307f559bd18b9f71dc41fbfedb88178478d2
Ryotaro <ryontaro.ishii@gmail.com>
2026-06-28 00:43:59 +0900

    Add Calculator sample

---
+++ practice/basic/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int x = 15;
+        int y = 7;
+
+        System.out.println("足し算 = " + (x + y));
+        System.out.println("引き算 = " + (x - y));
+        System.out.println("掛け算 = " + (x * y));
+        System.out.println("割り算 = " + (x / y));
+    }
+}
